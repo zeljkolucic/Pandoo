4b0b7b111faf82c1197d7aeb548f7a7bc2ec5be2
Zeljko Lucic <zeljko.lucic99@gmail.com>
2022-12-29 16:38:19 +0100

    Move gradient configuration to `layoutSubviews`

--- Pandoo/UI/Views/GradientView.swift
+++ Pandoo/UI/Views/GradientView.swift
@@ -8,22 +8,13 @@
 import UIKit
 
 final class GradientView: UIView {
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        configureGradient()
-    }
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        configureGradient()
-    }
-    
-    private func configureGradient() {
+    override func layoutSubviews() {
         let gradientLayer = CAGradientLayer()
         gradientLayer.frame = bounds
         gradientLayer.colors = [UIColor.systemBackground.cgColor, UIColor.systemBackground.withAlphaComponent(0.0).cgColor]
         gradientLayer.startPoint = CGPoint(x: 0.5, y: 1)
         gradientLayer.endPoint = CGPoint(x: 0.5, y: 0)
         layer.addSublayer(gradientLayer)
+        super.layoutSubviews()
     }
 }
